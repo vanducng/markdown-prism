72193d610a3c22da61907f7b2c3d40ee9d0a13df
Nguyen Van Duc <vanduc.dn@gmail.com>
2019-12-01 23:35:43 +0700

    Update extension.py

--- markdown_prism/extension.py
+++ markdown_prism/extension.py
@@ -15,11 +15,11 @@ class PrismCodeExtension(markdown.extensions.Extension):
 
 class PrismBlockPreprocessor(markdown.preprocessors.Preprocessor):
     PRISM_BLOCK_RE = re.compile(
-        r'(?P<fence>^(?:~{3,}|`{3,}))[ ]*(\{?\.?(?P<lang>[a-zA-Z0-9_+-|]*)\}?)?[ ]*\n(?P<code>.*?)(?<=\n)(?P=fence)[ ]*$',
+        r'(?P<fence>^(?:~{3,}|`{3,}))[ ]*(\{?\.?(?P<lang>[a-zA-Z0-9_+-]*)\}?)?[ ]*\n(?P<code>.*?)(?<=\n)(?P=fence)[ ]*$',
         re.MULTILINE | re.DOTALL
     )
     BLOCK_WRAP = '<pre><code{0}>{1}</code></pre>'
-    LANG_TAG = ' class="line-numbers language-{0}"'
+    LANG_TAG = ' class="language-{0}"'
     CODE_WRAP = '<code class="language-markup">{0}</code>'
 
     def __init__(self, md):
